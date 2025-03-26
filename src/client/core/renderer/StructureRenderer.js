c55e427832851cb75664a234821e7daf29dcddf2
Charlie <cbronson@gmail.com>
2025-03-26 15:09:51 -0500

    icrementally adding to buildings - set correct isometric view

--- src/client/core/renderer/StructureRenderer.js
+++ src/client/core/renderer/StructureRenderer.js
@@ -3,7 +3,7 @@ export class StructureRenderer {
         this.ctx = ctx;
         this.tileWidth = 64;
         this.tileHeight = 32;
-        this.heightScale = 32;
+        this.floorHeight = 32;
     }
 
     render(structure, worldX, worldY, screenX, screenY) {
@@ -12,66 +12,82 @@ export class StructureRenderer {
         this.ctx.save();
         
         const colors = {
-            frontRight: '#a0a0a0',  // SE face
-            frontLeft: '#808080',   // SW face
+            frontRight: '#a0a0a0',
+            frontLeft: '#808080',
             top: '#c0c0c0'
         };
 
-        this.drawSingleTileBox(
-            screenX,
-            screenY,
-            this.heightScale,  // Single floor height
-            colors
-        );
+        // For a structure at (x,y), the corners should be at:
+        // (x,y+h), (x+w,y+h), (x+w,y), (x,y)
+        // where w and h are the width and height of the structure
+        const basePoints = {
+            bottomLeft: { x: worldX, y: worldY + structure.height },
+            bottomRight: { x: worldX + structure.width, y: worldY + structure.height },
+            topRight: { x: worldX + structure.width, y: worldY },
+            topLeft: { x: worldX, y: worldY }
+        };
+
+        // Convert world coordinates to screen coordinates
+        const screenPoints = {
+            bottomLeft: this.worldToScreen(basePoints.bottomLeft.x, basePoints.bottomLeft.y),
+            bottomRight: this.worldToScreen(basePoints.bottomRight.x, basePoints.bottomRight.y),
+            topRight: this.worldToScreen(basePoints.topRight.x, basePoints.topRight.y),
+            topLeft: this.worldToScreen(basePoints.topLeft.x, basePoints.topLeft.y)
+        };
+
+        const totalHeight = (structure.template.floors || 1) * this.floorHeight;
+        
+        this.drawStructureBox(screenPoints, totalHeight, colors);
 
         this.ctx.restore();
     }
 
-    drawSingleTileBox(x, y, height, colors) {
-        // Define the diamond shape points for a single tile
-        const points = {
-            center: { x: x, y: y },
-            right: { x: x + this.tileWidth/2, y: y - this.tileHeight/2 },  // Changed to front right
-            left: { x: x - this.tileWidth/2, y: y - this.tileHeight/2 },   // Changed to front left
-            top: { x: x, y: y - height },
-            topRight: { x: x + this.tileWidth/2, y: y - this.tileHeight/2 - height },
-            topLeft: { x: x - this.tileWidth/2, y: y - this.tileHeight/2 - height }
+    worldToScreen(x, y) {
+        return {
+            x: (x - y) * (this.tileWidth / 2),
+            y: (x + y) * (this.tileHeight / 2)
         };
+    }
 
-        // Front-right face (NE)
+    drawStructureBox(points, height, colors) {
+        // Draw front-right face (SE)
         this.ctx.fillStyle = colors.frontRight;
         this.ctx.beginPath();
-        this.ctx.moveTo(points.center.x, points.center.y);
-        this.ctx.lineTo(points.right.x, points.right.y);
+        this.ctx.moveTo(points.bottomRight.x, points.bottomRight.y);
         this.ctx.lineTo(points.topRight.x, points.topRight.y);
-        this.ctx.lineTo(points.top.x, points.top.y);
+        this.ctx.lineTo(points.topRight.x, points.topRight.y - height);
+        this.ctx.lineTo(points.bottomRight.x, points.bottomRight.y - height);
         this.ctx.closePath();
         this.ctx.fill();
         this.ctx.stroke();
 
-        // Front-left face (NW)
+        // Draw front-left face (SW)
         this.ctx.fillStyle = colors.frontLeft;
         this.ctx.beginPath();
-        this.ctx.moveTo(points.center.x, points.center.y);
-        this.ctx.lineTo(points.left.x, points.left.y);
-        this.ctx.lineTo(points.topLeft.x, points.topLeft.y);
-        this.ctx.lineTo(points.top.x, points.top.y);
+        this.ctx.moveTo(points.bottomRight.x, points.bottomRight.y);
+        this.ctx.lineTo(points.bottomLeft.x, points.bottomLeft.y);
+        this.ctx.lineTo(points.bottomLeft.x, points.bottomLeft.y - height);
+        this.ctx.lineTo(points.bottomRight.x, points.bottomRight.y - height);
         this.ctx.closePath();
         this.ctx.fill();
         this.ctx.stroke();
 
-        // Top face
+        // Draw top face
         this.ctx.fillStyle = colors.top;
         this.ctx.beginPath();
-        this.ctx.moveTo(points.top.x, points.top.y);
-        this.ctx.lineTo(points.topRight.x, points.topRight.y);
-        this.ctx.lineTo(points.center.x, points.center.y - height);
-        this.ctx.lineTo(points.topLeft.x, points.topLeft.y);
+        this.ctx.moveTo(points.bottomRight.x, points.bottomRight.y - height);
+        this.ctx.lineTo(points.topRight.x, points.topRight.y - height);
+        this.ctx.lineTo(points.topLeft.x, points.topLeft.y - height);
+        this.ctx.lineTo(points.bottomLeft.x, points.bottomLeft.y - height);
         this.ctx.closePath();
         this.ctx.fill();
         this.ctx.stroke();
     }
 }
+
+
+
+
 
 
 
